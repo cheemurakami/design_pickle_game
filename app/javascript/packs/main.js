893a59b99f370ca3e831fb78476bd6fb73f30dee
Chisato Murakami <kiwipom1031@gmail.com>
2020-09-13 14:57:31 -0700

    add logic for matching and reseting

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -4,7 +4,7 @@ const imageUrls = [
   "https://cdn.filestackcontent.com/yEU3afnaR3izTbJ2yCNK", 
   "https://cdn.filestackcontent.com/2yPB3iM8RQ2clOOPZZNa"
 ]
-
+const hiddenImageUrl = "https://cdn.filestackcontent.com/mac0VBHdShaBLwOxpq66"
 function shuffle(arr) {
   for (let i = arr.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -17,14 +17,31 @@ const shuffledImages = shuffle(imageUrls.concat(imageUrls));
 
 //displaying hidden cards
 shuffledImages.map((_,i) => {
-  $(".card-wrapper").append(`<div class="card-container"><img class="card-image" data-position=${i} src="https://cdn.filestackcontent.com/mac0VBHdShaBLwOxpq66"></div>`)
+  $(".card-wrapper").append(`<div class="card-container"><img class="card-image" data-position=${i} src=${hiddenImageUrl}></div>`)
   if ((i + 1) == shuffledImages.length / 2){
     $(".card-wrapper").append('<div class="break"></div>')
   }
 })
 
+let guessedPositionNum = [];
 $(".card-image").click((e) => {
   let imageEl = e.target
   let imagePosition = $(imageEl).data("position");
   $(imageEl).attr("src", shuffledImages[imagePosition]);
+
+  guessedPositionNum.push(imagePosition); 
+  if (guessedPositionNum.length > 2) {
+    guessedPositionNum = [imagePosition];
+  }
+  let firstGuessedUrl = shuffledImages[guessedPositionNum[0]]
+  let secondGuessedUrl = shuffledImages[guessedPositionNum[1]]
+  if(firstGuessedUrl === secondGuessedUrl){
+    console.log("matched!")
+  } else if (guessedPositionNum.length == 2){
+    setTimeout(() => {
+      $(`.card-image[data-position="${guessedPositionNum[0]}"]`).attr("src", hiddenImageUrl);
+      $(`.card-image[data-position="${guessedPositionNum[1]}"]`).attr("src", hiddenImageUrl);
+    }, 2000)
+  }
 })
+
