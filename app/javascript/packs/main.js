9da93d178e2d297ec37e3625455d84e9f8a3681c
Chisato Murakami <kiwipom1031@gmail.com>
2020-09-13 11:58:12 -0700

    add data-position and shuffle image urls to get random images

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,14 +1,23 @@
-console.log('hello')
 
 $(".card-image").click((e) => {
   let imageEl = e.target
-  $(imageEl).attr("src", imageUrl[0])
+  let imagePosition = $(imageEl).data("position");
+  $(imageEl).attr("src", shuffledImages[imagePosition]);
 })
 
-const imageUrl = [
+const imageUrls = [
   "https://cdn.filestackcontent.com/0xpImnBSQ0i0sZ62RwHU", 
   "https://cdn.filestackcontent.com/5nFP5LXmRBKNtZPnSZoM", 
   "https://cdn.filestackcontent.com/yEU3afnaR3izTbJ2yCNK", 
   "https://cdn.filestackcontent.com/2yPB3iM8RQ2clOOPZZNa"
 ]
 
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+const shuffledImages = shuffle(imageUrls);
